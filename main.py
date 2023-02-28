201a32888a06ebe98079f732ca0f04bb84a65e25
sabeehalam <sabeeh_alam_96@hotmail.com>
2023-02-28 15:09:58 +0500

    Beginning

---
+++ main.py
@@ -0,0 +1,150 @@
+import ustruct
+from machine import UART, Pin
+import time
+
+# Define Modbus function codes
+READ_COILS = 0x01
+READ_DISCRETE_INPUTS = 0x02
+READ_HOLDING_REGISTERS = 0x03
+READ_INPUT_REGISTERS = 0x04
+WRITE_SINGLE_COIL = 0x05
+WRITE_SINGLE_REGISTER = 0x06
+# WRITE_MULTIPLE_COILS = 15
+# WRITE_MULTIPLE_REGISTERS = 16
+
+# Define slave address
+SLAVE_ADDRESS = 0x01
+
+# Define UART parameters
+UART_BAUD_RATE = 9600
+UART_DATA_BITS = 8
+UART_STOP_BITS = 1
+UART_PARITY = None
+# UART_TIMEOUT = 1000
+
+# Define Modbus registers and coils
+registers = bytearray(10)
+coils = bytearray(10)
+
+# Define Modbus RTU Registers
+coils = {
+    0x0001: 123,  # Read-Only Integer
+    0x0002: 456,  # Read-Write Integer
+    0x0003: False,  # Read-Write Boolean
+}
+
+registers = {
+    0x0001: 123,  # Read-Only Integer
+    0x0002: 456,  # Read-Write Integer
+    0x0003: False,  # Read-Write Boolean
+}
+
+holding_registers = {
+    0x0001: 123,  # Read-Only Integer
+    0x0002: 456,  # Read-Write Integer
+    0x0003: False,  # Read-Write Boolean
+}
+
+input_registers = {
+    0x0001: 123,  # Read-Only Integer
+    0x0002: 456,  # Read-Write Integer
+    0x0003: False,  # Read-Write Boolean
+}
+
+# Initialize UART
+uart = UART(1, baudrate=UART_BAUD_RATE)
+uart.init(baudrate=UART_BAUD_RATE, bits=UART_DATA_BITS,
+            stop=UART_STOP_BITS, parity=UART_PARITY)
+
+# Function to handle Modbus requests
+def handle_request(data):
+
+    # Extract function code and register/coil address
+    slave_address, function_code, start_register, register_count = ustruct.unpack(">BBBB", data)
+    
+    print("Slave address: ", slave_address)
+    print("function_code: ", function_code)
+    print("start_register: ", start_register)
+    print("register_count: ", register_count)
+    
+    # Check slave address
+    if slave_address != SLAVE_ADDRESS:
+        return None
+    
+    # Handle read coil request
+    if function_code == READ_COILS:
+        # Extract coils
+        response_data = ustruct.pack(">BB", start_register, function_code)
+        for i in range(register_count):
+            response_data[i // 8] |= (coils[start_register + i] << (i % 8))
+        return response_data
+
+    # Handle read holding register request
+    elif function_code == READ_HOLDING_REGISTERS:
+        # Extract registers
+        response_data = ustruct.pack(">BB", start_register, function_code)
+        for i in range(register_count):
+            value = holding_registers[2 * (start_register + i):2 * (start_register + i + 1)]
+            response_data[2 * i:2 * (i + 1)] = value
+        return response_data
+    
+        # Handle read holding register request
+    elif function_code == READ_INPUT_REGISTERS:
+        # Extract registers
+        response_data = ustruct.pack(">BB", start_register, function_code)
+        for i in range(register_count):
+            value = input_registers[2 * (start_register + i):2 * (start_register + i + 1)]
+            response_data[2 * i:2 * (i + 1)] = value
+        return response_data
+
+    # Handle write single coil request
+    elif function_code == WRITE_SINGLE_COIL:
+        # Extract coil value
+        coil_value = data[5] != 0x00
+        coils[start_register] = coil_value
+        return data[0:6]
+
+    # Handle write single register request
+    elif function_code == WRITE_SINGLE_REGISTER:
+        # Extract register value
+        register_value = ustruct.unpack(">H", data[4:6])[0]
+        registers[2 * start_register:2 * (start_register + 1)] = ustruct.pack(">H", register_value)
+        return data[0:6]
+
+    # Handle write multiple coils request
+    elif function_code == WRITE_MULTIPLE_COILS:
+        # Extract coils
+        coil_count = register_count
+        coil_data = data[7:]
+        for i in range(coil_count):
+            coils[start_register + i] = (coil_data[i // 8] >> (i % 8)) & 0x01
+        return data[0:6]
+
+    # Handle write multiple registers request
+    elif function_code == WRITE_MULTIPLE_REGISTERS:
+        # Extract registers
+        register_data[7:]
+    for i in range(register_count):
+        value = register_data[2 * i:2 * (i + 1)]
+        registers[2 * (start_register + i):2 * (start_register + i + 1)] = value
+        return data[0:6]
+
+    # Unsupported function code
+    else:
+        return None
+try:
+    while True:
+        # Wait for Modbus request
+        response_data = 0
+        data = uart.read(256)
+        if data is not None:
+            # Handle request
+            response_data = handle_request(data)
+        if response_data is not None:
+            # Send response
+            response = bytearray(SLAVE_ADDRESS + response_data)
+            uart.write(response)
+            time.sleep(1)
+except KeyboardInterrupt as e:
+    print("No more modbus")
+
